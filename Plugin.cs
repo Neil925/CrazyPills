67b7d3c8fdd0925c8b5ca42246167178c7a2f64d
Neil <elkadineil@gmail.com>
2021-07-03 02:59:31 -0400

    Updating to Exiled 2.10.0

--- Plugin.cs
+++ Plugin.cs
@@ -15,9 +15,9 @@ namespace CrazyPills
 
         public override string Prefix { get; } = "cp";
 
-        public override Version Version { get; } = new Version(1, 0, 0);
+        public override Version Version { get; } = new Version(1, 1, 0);
 
-        public override Version RequiredExiledVersion { get; } = new Version(2, 8, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(2, 10, 0);
 
         public static Plugin Instance;
 
