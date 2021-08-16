39a2c30d8ad150f88714bbbb4849347560b303b2
Neil <elkadineil@gmail.com>
2021-08-16 16:47:26 -0400

    ack

--- Plugin.cs
+++ Plugin.cs
@@ -15,7 +15,7 @@ namespace CrazyPills
 
         public override string Prefix { get; } = "cp";
 
-        public override Version Version { get; } = new Version(1, 4, 0);
+        public override Version Version { get; } = new Version(1, 4, 1);
 
         public override Version RequiredExiledVersion { get; } = new Version(2, 12, 0);
 
